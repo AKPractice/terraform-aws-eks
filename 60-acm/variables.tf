25a3678c27492668592b43261fe956197a9c9c3c
Nelluri Arun Kumar <nelluri.arunkumar@gmail.com>
2026-08-02 17:09:43 +0530

    Creating the AWS EKS through Terraform

---
+++ 60-acm/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    default = "roboshop"  
+}
+
+variable "environment" {
+    default = "dev" 
+}
+
+variable "zone_id" {
+    default = "Z0173021E9YTKU78MGR" 
+}
+
+variable "zone_name" {
+    default = "arunkumarnelluri.site" 
+}
